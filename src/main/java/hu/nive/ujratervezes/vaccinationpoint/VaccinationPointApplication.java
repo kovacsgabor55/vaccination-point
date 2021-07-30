e41c83582d8440fa86f723cc4645455f60977fd0
ShadowDragon <kovacsgabor55@gmail.com>
2021-07-30 11:35:43 +0200

    Initial commit

---
+++ src/main/java/hu/nive/ujratervezes/vaccinationpoint/VaccinationPointApplication.java
@@ -0,0 +1,13 @@
+package hu.nive.ujratervezes.vaccinationpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinationPointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaccinationPointApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/nive/ujratervezes/vaccinationpoint/VaccinationPointApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.nive.ujratervezes.vaccinationpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationPointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
